c2a4400979fe2e421f6d10ab52211a5fb27e4783
Mohammad Amin Zeynali <office_zeinaly@yahoo.com>
2018-03-20 15:21:35 +0330

    added Home controller , main page

---
+++ Music.Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Music.Core.Interfaces;
+using Music.Infrastructure.Repository;
+
+namespace Music.Web.Controllers
+{
+	[AllowAnonymous]
+	[RoutePrefix("")]
+	public class HomeController : Controller
+	{
+		private readonly IGenreRepository _genreRepository;
+		private readonly IBandRepository _bandRepository;
+
+		public HomeController()
+			:this(new GenreRepository(),new BandRepository())
+		{
+		}
+
+		public HomeController(IGenreRepository genreRepository,IBandRepository bandRepository)
+		{
+			_genreRepository = genreRepository;
+			_bandRepository = bandRepository;
+		}
+
+
+        // GET: Home
+        public async Task<ActionResult> Index()
+        {
+	        ViewBag.ListGenres = await _genreRepository.GetAllyAsync();
+
+            return View();
+        }
+    }
+}
